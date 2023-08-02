ce577952f54fbc51b695792c4fe7d64bb797c3db
James Douglass <douggynix@gmail.com>
2023-08-02 01:02:27 -0400

    Adding Validator
    Adding Validator

--- index.js
+++ index.js
@@ -1,86 +1,90 @@
-const Note = require('./model/note')
+const Note = require("./model/note")
 
-const express = require('express')
+const process = require("node:process")
+
+const express = require("express")
 const app = express()
 
-const cors = require('cors')
+const cors = require("cors")
 
 app.use(cors())
 
 app.use(express.json())
 
-app.use(express.static('build'))
+app.use(express.static("build"))
+
+const errorHandler = (error, request, response, next) => {
+  console.error("ErrorHandler: ",error.message)
+  
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {    return response.status(400).json({ error: error.message })  }
+  
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const requestLogger =(request,response,next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log("Method:", request.method)
+  console.log("Path:  ", request.path)
+  console.log("Body:  ", request.body)
+  console.log("---")
+  next()
 }
 
 app.use(requestLogger)
 
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-
-app.get('/api/notes',(request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-      })       
+app.get("/api/notes",(request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })       
 })
 
-app.get('/api/notes/:id',(request, response) =>  {
-    Note.findById(request.params.id).then( note => {
-        response.json(note)
-    })
+app.get("/api/notes/:id",(request, response) =>  {
+  Note.findById(request.params.id).then( note => {
+    response.json(note)
+  })
 })
 
-const generateId = () => {
 
-    console.log("notes.map(n => n.id) : ",notes.map(n => n.id))
-    console.log("...notes.map(n => n.id) : ",...notes.map(n => n.id))
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
+app.post("/api/notes", (request,response) =>{
+  const body = request.body
+  console.log(body)
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(savedNote =>{
+    response.json(savedNote)
+  })  
+})
 
 
-app.post('/api/notes', (request,response) =>{
-    const body = request.body
-    console.log(body)
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
+app.put("/api/notes/:id", (request,response,next) =>{
+  const {content,important} = request.body
+
+  console.log("update note with id",request.params.id, " and body", request.body)
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content,important},
+    {new: true, runValidators: true, context: "query"}
+  ).then(updatedNote =>{
+    response.json(updatedNote)
+  })
+    .catch(error => {
+      console.log("Failed Updating :",request.params.id,error)
+      next(error)
     })
-    note.save().then(savedNote =>{
-        response.json(savedNote)
-    })  
 })
 
 
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 
--- model/note.js
+++ model/note.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
+const process = require("node:process")
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
 
 const url = process.env.MONGODB_URI
 
 
-console.log('connecting to', url)
+console.log("connecting to", url)
 
 mongoose.connect(url)
 
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    console.log("error connecting to MongoDB:", error.message)
   })
 
 const noteSchema = new mongoose.Schema({
@@ -22,7 +23,7 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -30,4 +31,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema)
